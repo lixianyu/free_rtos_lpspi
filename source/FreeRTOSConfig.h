f5feb22ad1a246838c95d1d3411ff42254303b43
lixianyu <lbonus@163.com>
2019-06-16 18:04:42 +0800

    Success : W25Q Flash : Read JEDEC ID (9Fh)

--- source/FreeRTOSConfig.h
+++ source/FreeRTOSConfig.h
@@ -42,9 +42,9 @@
 #define configUSE_PREEMPTION                    1
 #define configUSE_TICKLESS_IDLE                 0
 #define configCPU_CLOCK_HZ                      (SystemCoreClock)
-#define configTICK_RATE_HZ                      ((TickType_t)200)
+#define configTICK_RATE_HZ                      ((TickType_t)1000)
 #define configMAX_PRIORITIES                    5
-#define configMINIMAL_STACK_SIZE                ((unsigned short)90)
+#define configMINIMAL_STACK_SIZE                ((unsigned short)130)
 #define configMAX_TASK_NAME_LEN                 20
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
@@ -68,7 +68,7 @@
 /* Memory allocation related definitions. */
 #define configSUPPORT_STATIC_ALLOCATION         0
 #define configSUPPORT_DYNAMIC_ALLOCATION        1
-#define configTOTAL_HEAP_SIZE                   ((size_t)(10 * 1024))
+#define configTOTAL_HEAP_SIZE                   ((size_t)(71 * 1024))
 #define configAPPLICATION_ALLOCATED_HEAP        0
 
 /* Hook function related definitions. */
--- source/freertos_hello.c
+++ source/freertos_hello.c
@@ -15,24 +15,306 @@
 /* Freescale includes. */
 #include "fsl_device_registers.h"
 #include "fsl_debug_console.h"
+#include "fsl_lpspi_freertos.h"
+
 #include "board.h"
 
 #include "pin_mux.h"
 #include "clock_config.h"
+
+
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
+    
+#define ADS8668_CMD_NO_OP     0x00
+#define ADS8668_CMD_STDBY     0x82
+#define ADS8668_CMD_PWR_DN    0x83
+#define ADS8668_CMD_RST       0x85
+#define ADS8668_CMD_AUTO_RST  0xA0
+#define ADS8668_CMD_MAN_CH0   0xC0
+#define ADS8668_CMD_MAN_CH1   0xC4
+#define ADS8668_CMD_MAN_CH2   0xC8
+#define ADS8668_CMD_MAN_CH3   0xCC
+#define ADS8668_CMD_MAN_CH4   0xD0
+#define ADS8668_CMD_MAN_CH5   0xD4
+#define ADS8668_CMD_MAN_CH6   0xD8
+#define ADS8668_CMD_MAN_CH7   0xDC
+#define ADS8668_CMD_MAN_AUX   0xE0
+    
+    
+#define ADS8668_PRG_AUTO_SEQ_EN  0x01
+#define ADS8668_PRG_CH_PWR_DN    0x02
+    
+#define ADS8668_PRG_FEATURE_SEL  0x03
+#define ADS8668_PRG_RANGE_CH0    0x05
+    
+    /* ALARM FLAG REGISTERS (Read-Only) */
+#define ADS8668_PRG_TRIP_ALL     0x10 //ALARM Overview Tripped-Flag
+#define ADS8668_PRG_TRIP0        0x11 //ALARM Ch 0-3 Tripped-Flag
+#define ADS8668_PRG_ACTIVE0      0x12 //ALARM Ch 0-3 Active-Flag
+#define ADS8668_PRG_TRIP1        0x13 //ALARM Ch 4-7 Tripped-Flag
+#define ADS8668_PRG_ACTIVE1      0x14 //ALARM Ch 4-7 Active-Flag
+    
+    /* ALARM THRESHOLD REGISTERS */
+#define ADS8668_PRG_HYSTERESIS_CH0         0x15 //Ch 0 Hysteresis
+#define ADS8668_PRG_HIGH_THRESHOLD_MSB_CH0 0x16
+#define ADS8668_PRG_HIGH_THRESHOLD_LSB_CH0 0x17
+#define ADS8668_PRG_LOW_THRESHOLD_MSB_CH0  0x18
+#define ADS8668_PRG_LOW_THRESHOLD_LSB_CH0  0x19
+    
+#define ADS8668_PRG_HYSTERESIS_CH1         0x1A //Ch 1 Hysteresis
+#define ADS8668_PRG_HIGH_THRESHOLD_MSB_CH1 0x1B
+#define ADS8668_PRG_HIGH_THRESHOLD_LSB_CH1 0x1C
+#define ADS8668_PRG_LOW_THRESHOLD_MSB_CH1  0x1D
+#define ADS8668_PRG_LOW_THRESHOLD_LSB_CH1  0x1E
+    
+#define ADS8668_PRG_HYSTERESIS_CH2         0x1F //Ch 2 Hysteresis
+#define ADS8668_PRG_HIGH_THRESHOLD_MSB_CH2 0x20
+#define ADS8668_PRG_HIGH_THRESHOLD_LSB_CH2 0x21
+#define ADS8668_PRG_LOW_THRESHOLD_MSB_CH2  0x22
+#define ADS8668_PRG_LOW_THRESHOLD_LSB_CH2  0x23
+    
+#define ADS8668_PRG_HYSTERESIS_CH3         0x24 //Ch 3 Hysteresis
+#define ADS8668_PRG_HIGH_THRESHOLD_MSB_CH3 0x25
+#define ADS8668_PRG_HIGH_THRESHOLD_LSB_CH3 0x26
+#define ADS8668_PRG_LOW_THRESHOLD_MSB_CH3  0x27
+#define ADS8668_PRG_LOW_THRESHOLD_LSB_CH3  0x28
+    
+#define ADS8668_PRG_HYSTERESIS_CH4         0x29 //Ch 4 Hysteresis
+#define ADS8668_PRG_HIGH_THRESHOLD_MSB_CH4 0x2A
+#define ADS8668_PRG_HIGH_THRESHOLD_LSB_CH4 0x2B
+#define ADS8668_PRG_LOW_THRESHOLD_MSB_CH4  0x2C
+#define ADS8668_PRG_LOW_THRESHOLD_LSB_CH4  0x2D
+    
+#define ADS8668_PRG_HYSTERESIS_CH5         0x2E //Ch 5 Hysteresis
+#define ADS8668_PRG_HIGH_THRESHOLD_MSB_CH5 0x2F
+#define ADS8668_PRG_HIGH_THRESHOLD_LSB_CH5 0x30
+#define ADS8668_PRG_LOW_THRESHOLD_MSB_CH5  0x31
+#define ADS8668_PRG_LOW_THRESHOLD_LSB_CH5  0x32
+    
+#define ADS8668_PRG_HYSTERESIS_CH6         0x33 //Ch 6 Hysteresis
+#define ADS8668_PRG_HIGH_THRESHOLD_MSB_CH6 0x34
+#define ADS8668_PRG_HIGH_THRESHOLD_LSB_CH6 0x35
+#define ADS8668_PRG_LOW_THRESHOLD_MSB_CH6  0x36
+#define ADS8668_PRG_LOW_THRESHOLD_LSB_CH6  0x37
+    
+#define ADS8668_PRG_HYSTERESIS_CH7         0x38 //Ch 7 Hysteresis
+#define ADS8668_PRG_HIGH_THRESHOLD_MSB_CH7 0x39
+#define ADS8668_PRG_HIGH_THRESHOLD_LSB_CH7 0x3A
+#define ADS8668_PRG_LOW_THRESHOLD_MSB_CH7  0x3B
+#define ADS8668_PRG_LOW_THRESHOLD_LSB_CH7  0x3C
+
+#define KALYKE_SPI_BASE    LPSPI1
+    /* Select USB1 PLL PFD0 (720 MHz) as lpspi clock source */
+#define KALYKE_LPSPI_CLOCK_SOURCE_SELECT (1U)
+    /* Clock divider for master lpspi clock source */
+#define KALYKE_LPSPI_CLOCK_SOURCE_DIVIDER (7U)
+    
+#define KALYKE_LPSPI_MASTER_CLK_FREQ (CLOCK_GetFreq(kCLOCK_Usb1PllPfd0Clk) / (KALYKE_LPSPI_CLOCK_SOURCE_DIVIDER + 1U))
+#define KALYKE_LPSPI_TRANSFER_BAUDRATE 1000000U
+#define KALYKE_LPSPI_MASTER_PCS_FOR_INIT (kLPSPI_Pcs0)
 
 /* Task priorities. */
 #define hello_task_PRIORITY (configMAX_PRIORITIES - 1)
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
-static void hello_task(void *pvParameters);
+
+void test_lpspi_read(void);
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+lpspi_rtos_handle_t gSpiRtosHandle;
+
+static uint8_t g_tx_buffer[64];
+static uint8_t g_rx_buffer[64];
 
 /*******************************************************************************
  * Code
  ******************************************************************************/
+
+void hexdump(const void *p, size_t size)
+{
+#if SDK_DEBUGCONSOLE == DEBUGCONSOLE_REDIRECT_TO_SDK
+
+    const uint8_t *c = p;
+
+    PRINTF("Dumping %u bytes from %p:\r\n", size, p);
+
+    while (size > 0)
+    {
+        unsigned i;
+
+        for (i = 0; i < 16; i++)
+        {
+            if (i < size)
+                PRINTF("%02X ", c[i]);
+            else
+                PRINTF("   ");
+        }
+#if 0
+        for (i = 0; i < 16; i++)
+        {
+            if (i < size)
+                PRINTF("%c", c[i] >= 32 && c[i] < 127 ? c[i] : '.');
+            else
+                PRINTF(" ");
+        }
+#endif
+        PRINTF("\r\n");
+
+        c += 16;
+
+        if (size <= 16)
+            break;
+
+        size -= 16;
+    }
+    PRINTF("\r\n");
+#endif
+}
+
+// W25Q Flash : Read JEDEC ID (9Fh)
+void test_lpspi_read4(void)
+{
+    PRINTF("Read JEDEC ID (9Fh)\r\n");
+    memset(g_rx_buffer, 0, sizeof(g_rx_buffer));
+    memset(g_tx_buffer, 0, sizeof(g_tx_buffer));
+    g_tx_buffer[0] = 0x9F;
+    PRINTF("g_tx_buffer[0] = 0x%X\r\n", g_tx_buffer[0]);
+    //g_tx_buffer[0] <<= 1;
+    lpspi_transfer_t spi_transfer;
+    spi_transfer.txData = g_tx_buffer;
+    spi_transfer.rxData = g_rx_buffer;
+    spi_transfer.dataSize = 4;
+    //spi_transfer.configFlags = kLPSPI_MasterPcs0 | kLPSPI_MasterPcsContinuous | kLPSPI_MasterByteSwap;
+    //spi_transfer.configFlags = kLPSPI_MasterPcs0 | kLPSPI_MasterByteSwap;
+    spi_transfer.configFlags = kLPSPI_MasterPcs0 | kLPSPI_MasterPcsContinuous;
+    status_t ret = LPSPI_MasterTransferBlocking(KALYKE_SPI_BASE, &spi_transfer);
+    PRINTF("LPSPI_MasterTransferBlocking return : %d\r\n", ret);
+    hexdump(g_rx_buffer, sizeof(g_rx_buffer));
+}
+
+// W25Q Flash : Read Manufacturer / Device ID (90h)
+void test_lpspi_read3(void)
+{
+    PRINTF("Read Manufacturer / Device ID (90h)\r\n");
+    memset(g_rx_buffer, 0, sizeof(g_rx_buffer));
+    memset(g_tx_buffer, 0, sizeof(g_tx_buffer));
+    g_tx_buffer[0] = 0x90;
+    PRINTF("g_tx_buffer[0] = 0x%X\r\n", g_tx_buffer[0]);
+    //g_tx_buffer[0] <<= 1;
+    lpspi_transfer_t spi_transfer;
+    spi_transfer.txData = g_tx_buffer;
+    spi_transfer.rxData = g_rx_buffer;
+    spi_transfer.dataSize = 6;
+    //spi_transfer.configFlags = kLPSPI_MasterPcs0 | kLPSPI_MasterPcsContinuous | kLPSPI_MasterByteSwap;
+    //spi_transfer.configFlags = kLPSPI_MasterPcs0 | kLPSPI_MasterByteSwap;
+    spi_transfer.configFlags = kLPSPI_MasterPcs0 | kLPSPI_MasterPcsContinuous;
+    status_t ret = LPSPI_MasterTransferBlocking(KALYKE_SPI_BASE, &spi_transfer);
+    PRINTF("LPSPI_MasterTransferBlocking return : %d\r\n", ret);
+    hexdump(g_rx_buffer, sizeof(g_rx_buffer));
+}
+
+
+// W25Q Flash : Read Unique ID Number (4Bh)
+void test_lpspi_read2(void)
+{
+    PRINTF("Read Unique ID Number (4Bh)\r\n");
+    memset(g_rx_buffer, 0, sizeof(g_rx_buffer));
+    memset(g_tx_buffer, 0, sizeof(g_tx_buffer));
+    g_tx_buffer[0] = 0x4B;
+    PRINTF("g_tx_buffer[0] = 0x%X\r\n", g_tx_buffer[0]);
+    //g_tx_buffer[0] <<= 1;
+    lpspi_transfer_t spi_transfer;
+    spi_transfer.txData = g_tx_buffer;
+    spi_transfer.rxData = g_rx_buffer;
+    spi_transfer.dataSize = 13;
+    //spi_transfer.configFlags = kLPSPI_MasterPcs0 | kLPSPI_MasterPcsContinuous | kLPSPI_MasterByteSwap;
+    //spi_transfer.configFlags = kLPSPI_MasterPcs0 | kLPSPI_MasterByteSwap;
+    spi_transfer.configFlags = kLPSPI_MasterPcs0 | kLPSPI_MasterPcsContinuous;
+    status_t ret = LPSPI_MasterTransferBlocking(KALYKE_SPI_BASE, &spi_transfer);
+    PRINTF("LPSPI_MasterTransferBlocking return : %d\r\n", ret);
+    hexdump(g_rx_buffer, sizeof(g_rx_buffer));
+}
+
+void test_lpspi_read(void)
+{
+    memset(g_rx_buffer, 0, sizeof(g_rx_buffer));
+    memset(g_tx_buffer, 0, sizeof(g_tx_buffer));
+    g_tx_buffer[0] = ADS8668_PRG_HIGH_THRESHOLD_MSB_CH0 << 1;
+    PRINTF("g_tx_buffer[0] = 0x%X\r\n", g_tx_buffer[0]);
+    //g_tx_buffer[0] <<= 1;
+    lpspi_transfer_t spi_transfer;
+    spi_transfer.txData = g_tx_buffer;
+    spi_transfer.rxData = g_rx_buffer;
+    spi_transfer.dataSize = 2;
+    spi_transfer.configFlags = kLPSPI_MasterPcs0 | kLPSPI_MasterPcsContinuous | kLPSPI_MasterByteSwap;
+    status_t ret = LPSPI_MasterTransferBlocking(KALYKE_SPI_BASE, &spi_transfer);
+    PRINTF("LPSPI_MasterTransferBlocking return : %d\r\n", ret);
+    hexdump(g_rx_buffer, sizeof(g_rx_buffer));
+}
+
+void kalyke_lpspi_init_ADS8668(void)
+{
+    PRINTF("Enter %s()\r\n", __func__);
+    NVIC_SetPriority(LPSPI3_IRQn, 5);
+
+    /*Set clock source for LPSPI*/
+    CLOCK_SetMux(kCLOCK_LpspiMux, KALYKE_LPSPI_CLOCK_SOURCE_SELECT);
+    CLOCK_SetDiv(kCLOCK_LpspiDiv, KALYKE_LPSPI_CLOCK_SOURCE_DIVIDER);
+
+    uint32_t srcClock_Hz = KALYKE_LPSPI_MASTER_CLK_FREQ;
+    PRINTF("srcClock_Hz = %u\r\n", srcClock_Hz);
+    lpspi_master_config_t masterConfig;
+    /*Master config*/
+    masterConfig.baudRate = KALYKE_LPSPI_TRANSFER_BAUDRATE;
+    masterConfig.bitsPerFrame = 8;
+    masterConfig.cpol = kLPSPI_ClockPolarityActiveLow;
+    masterConfig.cpha = kLPSPI_ClockPhaseSecondEdge;
+    masterConfig.direction = kLPSPI_MsbFirst;
+
+    masterConfig.pcsToSckDelayInNanoSec = 1000000000 / masterConfig.baudRate;
+    //masterConfig.pcsToSckDelayInNanoSec = 0;
+    masterConfig.lastSckToPcsDelayInNanoSec = 1000000000 / masterConfig.baudRate;
+    //masterConfig.lastSckToPcsDelayInNanoSec = 0;
+    masterConfig.betweenTransferDelayInNanoSec = 1000000000 / masterConfig.baudRate;
+    //masterConfig.betweenTransferDelayInNanoSec = 0;
+    
+    masterConfig.whichPcs = KALYKE_LPSPI_MASTER_PCS_FOR_INIT;
+    masterConfig.pcsActiveHighOrLow = kLPSPI_PcsActiveLow;
+
+    masterConfig.pinCfg = kLPSPI_SdiInSdoOut;
+    masterConfig.dataOutConfig = kLpspiDataOutTristate;
+
+    LPSPI_MasterInit(KALYKE_SPI_BASE, &masterConfig, srcClock_Hz);
+
+    //test_lpspi_read();
+    test_lpspi_read2();
+    test_lpspi_read3();
+    test_lpspi_read4();
+}
+
+/*!
+ * @brief Task responsible for printing of "Hello world." message.
+ */
+static void hello_task(void *pvParameters)
+{
+    PRINTF("hello_task RUN. Free heap size is %d bytes\r\n", xPortGetFreeHeapSize());
+
+    vTaskDelay(4000);
+    kalyke_lpspi_init_ADS8668();
+    for (;;)
+    {
+        PRINTF("Hello world.\r\n");
+        vTaskDelay(100000);
+        test_lpspi_read2();
+    }
+}
+
+
 /*!
  * @brief Application entry point.
  */
@@ -54,14 +336,3 @@ int main(void)
         ;
 }
 
-/*!
- * @brief Task responsible for printing of "Hello world." message.
- */
-static void hello_task(void *pvParameters)
-{
-    for (;;)
-    {
-        PRINTF("Hello world.\r\n");
-        vTaskSuspend(NULL);
-    }
-}
